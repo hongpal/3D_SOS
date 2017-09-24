ef7eb43080828969c2505ff6d20d78add5ab7ff1
김수봉 <godsavekin@naver.com>
2017-09-24 21:50:00 +0900

    자이로센서

---
+++ touchMoving/Assets/gyroScope.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyroScope : MonoBehaviour {
+    Vector3 gyroscope_rotation;
+ 
+    void Awake()
+    {
+        Input.gyro.enabled = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        gyroscope_rotation.x += Input.gyro.rotationRateUnbiased.x;
+        gyroscope_rotation.y += Input.gyro.rotationRateUnbiased.y;
+    }
+}
+
+
+ 
