c0d7b64a6f950ff7a04fa9bbcc7af3049643a839
김태한-PC\김태한 <jaragun1@naver.com>
2017-09-30 15:18:46 +0900

    다면체 애니메이션 구현 시도중

--- work/Assets/Sript/test.cs
+++ work/Assets/Sript/test.cs
@@ -12,7 +12,7 @@ public class test : MonoBehaviour {
         mesh.Clear();
         mesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0) };
         mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
-        mesh.triangles = new int[] { 0, 1, 2 };
+        mesh.triangles = new int[] { 0, 0, 5 };
 
     }
 	
