be1988b33f2914ea2fabd1648674358dba27cf4d
김태한-PC\김태한 <jaragun1@naver.com>
2017-11-17 20:59:27 +0900

    통신이 되긴하나 소리가 깨짐

--- touchMoving/Assets/ButtonX/Sript/NetworkManager.cs
+++ touchMoving/Assets/ButtonX/Sript/NetworkManager.cs
@@ -55,6 +55,77 @@ public class NetworkManager : MonoBehaviour {
         }
     }
 
+
+    void FixedUpdate()
+    {
+        // If there is a connection
+        if (Network.connections.Length > 0)
+        {
+            if (notRecording)
+            {
+                notRecording = false;
+                sendingClip = Microphone.Start(null, true, 100, 44100);
+                sending = true;
+            }
+            else if (sending)
+            {
+                int pos = Microphone.GetPosition(null);
+                int diff = pos - lastSample;
+
+                if (diff > 0)
+                {
+                    float[] samples = new float[diff * sendingClip.channels];
+                    sendingClip.GetData(samples, lastSample);
+                    byte[] ba = ToByteArray(samples);
+                    if (Network.isClient)
+                        GetComponent<NetworkView>().RPC("Send", RPCMode.Server, ba, sendingClip.channels);
+                    else
+                        GetComponent<NetworkView>().RPC("Send", RPCMode.Others, ba, sendingClip.channels);
+                }
+                lastSample = pos;
+            }
+        }
+    }
+
+    [RPC]
+    public void Send(byte[] ba, int chan)
+    {
+        if (Network.isClient)
+            print("server-> client");
+        else
+            print("client-> server");
+        float[] f = ToFloatArray(ba);
+        audioSource.clip = AudioClip.Create("", f.Length, chan, 44100, true, false);
+        audioSource.clip.SetData(f, 0);
+        if (!audioSource.isPlaying) audioSource.Play();
+
+    }
+    // Used to convert the audio clip float array to bytes
+    public byte[] ToByteArray(float[] floatArray)
+    {
+        int len = floatArray.Length * 4;
+        byte[] byteArray = new byte[len];
+        int pos = 0;
+        foreach (float f in floatArray)
+        {
+            byte[] data = System.BitConverter.GetBytes(f);
+            System.Array.Copy(data, 0, byteArray, pos, 4);
+            pos += 4;
+        }
+        return byteArray;
+    }
+    // Used to convert the byte array to float array for the audio clip
+    public float[] ToFloatArray(byte[] byteArray)
+    {
+        int len = byteArray.Length / 4;
+        float[] floatArray = new float[len];
+        for (int i = 0; i < byteArray.Length; i += 4)
+        {
+            floatArray[i / 4] = System.BitConverter.ToSingle(byteArray, i);
+        }
+        return floatArray;
+    }
+
     public void StartServer()
     {
         num = Random.Range(0, 9999);
