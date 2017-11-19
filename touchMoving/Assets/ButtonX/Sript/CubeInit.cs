a094473132bb387877cb7cbf8df215d1b7345ceb
김태한-PC\김태한 <jaragun1@naver.com>
2017-11-19 22:32:36 +0900

    젠가 -2

--- touchMoving/Assets/ButtonX/Sript/CubeInit.cs
+++ touchMoving/Assets/ButtonX/Sript/CubeInit.cs
@@ -9,6 +9,7 @@ public class CubeInit : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
         for (int i = 0; i < 6; i++)
         {
             Cube[i] = GameObject.Find("Plane").transform.Find("Cube-" + (i + 1)).gameObject;
--- touchMoving/Assets/ButtonX/Sript/genga.cs
+++ touchMoving/Assets/ButtonX/Sript/genga.cs
@@ -3,48 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class genga : MonoBehaviour {
-    public GameObject[] block = new GameObject[30];
-    Vector3 v = new Vector3(-1, -1.7f, 5);
-    int block_count = 0;
+    public static GameObject[] block = new GameObject[30];
+    public static bool[] check = new bool[30];
+    bool check1 = true;
+    int k = 0;
+    private void Start()
+    {
+        for (int i = 0; i < 30; i++)
+            check[i] = false;
 
-	// Use this for initialization
-	void Start () {
-		for(int i = 0; i < 10; i++)
+        for (int i = 0; i < 30; i++)
         {
-            if(i % 2 == 0)
-            {
-                for(int k = 0; k < 3; k++)
-                {
-                    block[block_count] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    block[block_count].transform.position = v;
-                    block[block_count].transform.localScale = new Vector3(1, 0.35f, 3);
-                    block_count++;
-                    v.x += 1.001f;
-                }
-            }
-            else
-            {
-                v.x = 0;
-                v.z = 4;
-                for (int k = 0; k < 3; k++)
-                {
-                    block[block_count] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    block[block_count].transform.position = v;
-                    block[block_count].transform.localScale = new Vector3(1, 0.35f, 3);
-                    block[block_count].transform.Rotate(0, 90, 0);
-                    block_count++;
-                    v.z += 1.001f;
-                }
-                v.x = -1;
-                v.z = 5;
-            }
-            
-            v.y += 0.351f;
+            block[i] = GameObject.Find("GenGa/Cube (" + i +")");
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+       
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.touchCount > 0 && check1)
+        {
+            Vector2 pos = Input.GetTouch(0).position;    // 터치한 위치
+
+            Vector3 theTouch = new Vector3(pos.x, pos.y, 0.0f);    // 변환 안하고 바로 Vector3로 받아도 되겠지.
+
+            Ray ray = Camera.main.ScreenPointToRay(theTouch);    // 터치한 좌표 레이로 바꾸엉
+
+            RaycastHit hit;    // 정보 저장할 구조체 만들고
+
+            if (Physics.Raycast(ray, out hit))    // 레이저를 끝까지 쏴블자. 충돌 한넘이 있으면 return true다.
+            {
+                Touch touch = Input.GetTouch(0);
+
+                if (Input.GetTouch(0).phase == TouchPhase.Began)    // 딱 처음 터치 할때 발생한다
+                {
+                    print(hit.transform.gameObject);
+
+                    for (int i = 0; i < 30; i++)
+                    {
+                        if (hit.transform.gameObject.name.Equals(block[i].name))
+                        {
+                            print("go");
+                            genga.check[i] = true;
+                            k = i;
+                            break;
+                        }
+                    }
+                    check1 = false;
+                    genga.block[k].GetComponent<MeshRenderer>().material.color = Color.black;
+                }
+
+                else if (Input.GetTouch(0).phase == TouchPhase.Moved)    // 터치하고 움직이믄 발생한다.
+                {
+
+                }
+
+                else if (Input.GetTouch(0).phase == TouchPhase.Ended)    // 터치 따악 떼면 발생한다.
+                {
+                    //this.GetComponent<MeshRenderer>().material.color = c;
+                }
+
+            }
+
+        }
+    }
 }
